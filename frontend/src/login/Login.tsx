ad9bb4b1d97e08da09de5634745f1526c9d14c60
Volhakov <olkakovka812@gmail.com>
2026-04-21 18:35:11 +0200

    created login page

---
+++ frontend/src/login/Login.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import "./login.css";
+
+export default function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const handleLogin = () => {
+        setError("");
+
+        if(!email || !password) {
+            setError("Wypełnij wszystkie pola!");
+            return;
+        }
+    };
+
+    return (
+        <div className="container">
+            <img src="/images/full_logo.png" alt="PassManager logo" className="top-logo" />
+            <div className="login-box">
+                <h1 className="logo">🔐 PassManager</h1>
+                <h2>Logowanie</h2>
+
+                <section className="login-input">
+                    <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+
+                <input
+                    type="password"
+                    placeholder="Hasło"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+
+                <button onClick={handleLogin}>Zaloguj się</button>
+                </section>
+
+                {error && <p className="error">{error}</p>}
+
+                <p className="register-link">
+                    Nie masz konta? <a href="/register">Zarejestruje się</a>
+                </p>
+            </div>
+        </div>
+    );
+}
